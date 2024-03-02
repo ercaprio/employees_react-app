15fae118c287c6a30fb26f5ad0939657cf3c002f
Erkin <ercaprio@gmail.com>
2024-03-02 22:24:55 +0600

    Practice properties on a project

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -3,12 +3,17 @@ import './employees-list.css'
 import EmployeesListItem from "../employess-list-item/employees-list-item";
 
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+	const elements = data.map(item => {
+		return (
+			<EmployeesListItem {...item}/>
+		)
+	});
+
 	return (
 		<ul className="app-list list-group">
-			<EmployeesListItem/>
-			<EmployeesListItem/>
-			<EmployeesListItem/>
+			{elements}
 		</ul>
 	);
 }
