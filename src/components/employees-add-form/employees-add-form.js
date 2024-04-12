1788255081617e625154324422697cba4c9e0181
Erkin <ercaprio@gmail.com>
2024-04-12 18:01:05 +0500

    Lifting state

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -17,15 +17,26 @@ class EmployeesAddForm extends Component{
             [e.target.name]: e.target.value
         })
     }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
  
 	render() {
+        const {onAdd} = this.props;
         const {name, salary} = this.state;
 
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?" 
@@ -40,7 +51,9 @@ class EmployeesAddForm extends Component{
                         onChange={this.onValueChange}/>
     
                     <button type="submit"
-                            className="btn btn-outline-light">Добавить</button>
+                            className="btn btn-outline-light"
+                            onSubmit={() => onAdd(name, salary)}>Добавить
+                    </button>
                 </form>
             </div>
         );
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,7 +4,7 @@ import EmployeesListItem from '../employess-list-item/employees-list-item';
 
 import './employees-list.css'
 
-const EmployeesList = ({data, onDelete}) => {
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
 
 	const elements = data.map(item => {
 		const {id, ...itemProps} = item;
@@ -12,10 +12,11 @@ const EmployeesList = ({data, onDelete}) => {
 			<EmployeesListItem 
 				key={id} 
 				{...itemProps}
-				onDelete={() => onDelete(id)}/>
+				onDelete={() => onDelete(id)}
+				onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>
 		)
 	});
-
+  
 	return (
 		<ul className="app-list list-group">
 			{elements}
